0f24c92dfdb8c5ba31361fbb9ad1d5904e53c06a
akramulfata10 <superlay582@gmail.com>
2023-08-16 15:28:25 +0700

    feature/5 koneksi to mysql and go get gorm/mysql

--- app/server.go
+++ app/server.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"os"
 
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"github.com/joho/godotenv"
 	"github.com/gorilla/mux"
 	"gorm.io/gorm"
@@ -23,8 +25,32 @@ type AppConfig struct {
 	AppPort string
 }
 
-func (server *Server) KoneksiDB(appConfig AppConfig) {
+type DbConfig struct {
+	DBHost string
+	DBUser string
+	DBPassword string
+	DBName string
+	DBPort string
+	DBDriver string
+}
+
+func (server *Server) KoneksiDB(appConfig AppConfig, dbConfig DbConfig) {
 	fmt.Println("Welcome To " + appConfig.AppName)
+
+	var err error
+
+	if dbConfig.DBDriver == "mysql" {
+		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBHost, dbConfig.DBPort, dbConfig.DBName)
+		server.DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	} else {
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", dbConfig.DBHost, dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBName, dbConfig.DBPort)
+		server.DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	}
+
+	if err != nil {
+		panic("Could not connect to database")
+	}
+	
 	server.Router = mux.NewRouter()
 	server.initializeRoutes()
 }
@@ -35,7 +61,7 @@ func (server *Server) Run(address string) {
 }
 
 func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
+	if value, berhasil := os.LookupEnv(key); berhasil {
 		return value
 	}
 	return fallback
@@ -44,6 +70,7 @@ func getEnv(key, fallback string) string {
 func Run() {
 	var server = Server{}
 	var appConfig = AppConfig{}
+	var dbConfig = DbConfig{}
 
 	err := godotenv.Load()
 	if err != nil {
@@ -54,6 +81,13 @@ func Run() {
 	appConfig.AppEnv = getEnv("APP_ENV", "development")
 	appConfig.AppPort = getEnv("APP_PORT","9191")
 
-	server.KoneksiDB(appConfig)
+	dbConfig.DBHost = getEnv("DB_HOST", "localhost")
+	dbConfig.DBUser = getEnv("DB_USER", "user")
+	dbConfig.DBPassword = getEnv("DB_PASSWORD", "")
+	dbConfig.DBName = getEnv("DB_NAME", "gotoko-db")
+	dbConfig.DBPort = getEnv("DB_PORT", "5432")
+	dbConfig.DBDriver = getEnv("DB_DRIVER", "postgres")
+
+	server.KoneksiDB(appConfig, dbConfig)
 	server.Run(":" + appConfig.AppPort)
 }
