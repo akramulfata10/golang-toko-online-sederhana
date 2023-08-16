36cee6ee117455539db5f4cd1f256b01384faf62
akramulfata10 <superlay582@gmail.com>
2023-08-16 13:29:04 +0700

    feature/3-load-env-file-configuration

--- app/server.go
+++ app/server.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/gorilla/mux"
 	"gorm.io/gorm"
+
 )
 
 type Server struct {
@@ -14,8 +17,14 @@ type Server struct {
 	Router *mux.Router
 }
 
-func (server *Server) KoneksiDB() {
-	fmt.Println("Welcome To Gotoko")
+type AppConfig struct {
+	AppName string
+	AppEnv string
+	AppPort string
+}
+
+func (server *Server) KoneksiDB(appConfig AppConfig) {
+	fmt.Println("Welcome To " + appConfig.AppName)
 	server.Router = mux.NewRouter()
 	server.initializeRoutes()
 }
@@ -25,8 +34,26 @@ func (server *Server) Run(address string) {
 	log.Fatal(http.ListenAndServe(address, server.Router))
 }
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
 func Run() {
-	var testKoneksi = Server{}
-	testKoneksi.KoneksiDB()
-	testKoneksi.Run(":9000")
+	var server = Server{}
+	var appConfig = AppConfig{}
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Failed to load .env file")
+	}
+
+	appConfig.AppName = getEnv("APP_NAME","gotoko")
+	appConfig.AppEnv = getEnv("APP_ENV", "development")
+	appConfig.AppPort = getEnv("APP_PORT","9191")
+
+	server.KoneksiDB(appConfig)
+	server.Run(":" + appConfig.AppPort)
 }
