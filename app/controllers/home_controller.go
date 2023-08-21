d7c111a112f70f719ca912edeb2d438d4d53ae98
akramulfata10 <superlay582@gmail.com>
2023-08-21 21:10:04 +0700

    fix go mod error

--- app/controllers/home_controller.go
+++ app/controllers/home_controller.go
@@ -3,8 +3,22 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/unrolled/render"
+
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Fprintf(w, "wellcome to home page gotoko")
+
+	render := render.New(render.Options{
+		Layout: "layout",
+	})
+
+	_ = render.HTML(w, http.StatusOK, "home", map[string]interface{}{
+		"title": "Home Title",
+		"body":  "Home Description",
+	})
+
 }
